a4e721b95b5b35c69686afad19cb9e5cff21bd68
Dileep Kumar <dileep.pdkr@gmail.com>
2019-04-21 22:48:35 +0530

    final result

--- number_to_word.rb
+++ number_to_word.rb
@@ -3,7 +3,7 @@ class NumberToWord
 	def convert_to_words phone
 		phone = phone.to_s
 		#return if number is not valid
-		if phone.nil? || !(phone !~ /\D/) || phone.size < 3 || phone.include?("1") || phone.include?('0')
+		if phone.nil? || !(phone !~ /\D/) || phone.size < 9 || phone.include?("1") || phone.include?('0')
 			puts "Provide a valid phone number"
     	return []
     end
@@ -18,18 +18,50 @@ class NumberToWord
     										"8" => ["t", "u", "v"],
     										"9" => ["w", "x", "y", "z"]
     									}
-mapping_dictionary = []
-file_path = "dictionary.txt"
-#fetch all values from dictionary
-File.foreach( file_path ) do |word|
-	mapping_dictionary << word.chop.to_s.downcase
-end
-keys = phone.chars.collect{|char|mapping_letters[char]}
-# puts keys
-mapping_result = keys.shift.product(*keys).map(&:join)
+		mapping_dictionary = {}
+		file_path = "dictionary.txt"
+		#fetch all values from dictionary and store into mapping_dictionary
+		File.foreach( file_path ) do |word|
+			mapping_dictionary[word.length] = [] unless mapping_dictionary.has_key?(word.length)
+			mapping_dictionary[word.length] << word.chop.to_s.downcase
+		end
 
-final_result =  mapping_result & mapping_dictionary
-puts final_result
+		keys = phone.chars.collect{|char|mapping_letters[char]}
+    total_digits = keys.length - 1
+    results = {}
+    for i in (2..total_digits)
+      first_array = keys[0..i]
+      next if first_array.length < 3
+      second_array = keys[i + 1..total_digits]
+      next if second_array.length < 3
+      first_combination = first_array.shift.product(*first_array).map(&:join) # Get product of arrays
+      next if first_combination.nil?
+      second_combination = second_array.shift.product(*second_array).map(&:join)
+      next if second_combination.nil?
+      # Get matched words from file
+      results[i] = [(first_combination & mapping_dictionary[i+2]), (second_combination & mapping_dictionary[total_digits - i +1])]
+    end
+
+    final_words = []
+    final_result = []
+    results.each do |key, combi|
+      next if combi.first.nil? || combi.last.nil?
+      combi.first.product(combi.last).map{|w| final_words << w}
+      final_result << combi.first.product(combi.last).map(&:join)
+    end
+    # Map all char for final result
+    final_words << (keys.shift.product(*keys).map(&:join) & mapping_dictionary[11]).join(", ")
+    final_result << (keys.shift.product(*keys).map(&:join) & mapping_dictionary[11]).map(&:join)
+    if final_words.size <= 0
+    	puts "no result Found"
+    else
+    	puts final_words
+    	puts '-=-=-=-=-'
+    	puts final_result
+    end
+
 	end
 end
-NumberToWord.new.convert_to_words 6686787825
+puts "Enter 10 digit numbers"
+phone = gets()
+NumberToWord.new().convert_to_words(phone.chop)
